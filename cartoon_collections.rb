40775896db771cd3818ffac3f39e78c07709133c
elijahmm17 <elijahmm17@gmail.com>
2019-05-15 21:49:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,13 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(calls)
-  
+  long_call = false 
+  calls.each do |call|
+    if call.length > 4
+      long_call = true 
+    end 
+  end 
+  long_call
 end
 
 def find_the_cheese# code an argument here
