3fa57f327d2ddf1d4352bc4054c3834a3c22e26d
elijahmm17 <elijahmm17@gmail.com>
2019-05-15 21:52:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,7 +22,8 @@ def long_planeteer_calls(calls)
   long_call
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(food_items)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  food_items.each do |food|
+    if cheese_types.
 end
